7c0649240f3773d72627af6464d64d53cd623c85
伯蚌 <1119885099@qq.com>
2020-12-16 08:33:07 +0800

    add files

---
+++ 2-day.py
@@ -0,0 +1,5 @@
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+ps = 'rstrip()删除末尾的空白'
+print(favorite_language)
+print(ps)
---
+++ hello_message.py
@@ -0,0 +1,4 @@
+'''message = "你好 宇宙，这是来自地球的消息！"
+print(message)'''
+message = "你收到了我的第一条消息了吗？"
+print(message)
