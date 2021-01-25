964e3be7ba35eca496a57471e8b73cd032204f05
伯蚌 <1119885099@qq.com>
2021-01-25 13:01:19 +0800

    'B站自动更新时间py'

---
+++ Automatic/auto.py
@@ -0,0 +1,17 @@
+import datetime
+
+# 获取时间
+curr_time = datetime.datetime.now()
+new_time = curr_time.strftime("%Y年%m月%d日")
+day = curr_time.strftime("%d")
+muth = curr_time.strftime("%m")
+print(new_time)
+print(day)
+new_str = f'2021年1月12日  --  {muth}月{day}日 目标   完成  \n{new_time}目标 \n1，早7点5分起床\n2，python学习两个章节 \n3，模拟选择题基础部分做一遍 \n4，在家运动30分钟'
+print(new_str)
+# 打开文件
+fo = open("xinxin.txt", "w+" ,encoding='utf-8')
+# 读取文件所有内容
+fo.write(new_str)
+# 关闭文件
+fo.close()
